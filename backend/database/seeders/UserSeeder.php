8ecb35c3f5d200b8aeb2ef72a8cd69551c7047b4
Matheuskocotem <Kocotemmatheus@gmail.com>
2024-11-02 04:50:45 -0300

    UserSeed

---
+++ backend/database/seeders/UserSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        User::create([
+            'name' => 'Admin User',
+            'email' => 'admin@example.com',
+            'cpf' => '12345678902',
+            'password' => Hash::make('password123'), 
+            'role' => 'admin',
+        ]);
+    }
+}
