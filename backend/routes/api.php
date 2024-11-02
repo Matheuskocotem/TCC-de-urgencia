445ec107b541c26b3574b5dd3f601c3e8f4136ca
Matheuskocotem <Kocotemmatheus@gmail.com>
2024-11-02 02:40:45 -0300

    routes backend com role

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -4,13 +4,17 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 
-    // Public routes
-    Route::post('/register', [AuthController::class, 'register']);
-    Route::post('/login', [AuthController::class, 'login']);
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
 
-    // Protected routes
-    Route::middleware('auth:sanctum')->group(function () {
+Route::middleware(['auth:sanctum'])->group(function () {
+    Route::middleware(['role:user'])->group(function () {
         Route::post('/logout', [AuthController::class, 'logout']);
         Route::put('/update/{id}', [AuthController::class, 'update']);
         Route::delete('/delete/{id}', [AuthController::class, 'delete']);
     });
+    Route::middleware(['role:admin'])->group(function () {
+        Route::get('/index', [AuthController::class, 'index']);
+    });
+});
+
