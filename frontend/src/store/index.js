e1c0bb520b9cbd014cac488e2d72ab860266b72f
Gustavo <gustavohlusiak@gmail.com>
2024-11-06 01:31:33 -0300

    Testes

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -23,15 +23,29 @@ const store = createStore({
     token: localStorage.getItem('token') || null,
     role: localStorage.getItem('role') || null,
   },
+  getters: {
+    isAuthenticated(state) {
+      return !!state.token;
+    }
+  },
   mutations: {
     setToken(state, token) {
+      console.log('Token sendo armazenado:', token); // Verifique se o token está sendo armazenado corretamente
       state.token = token;
       localStorage.setItem('token', token);
-      axios.defaults.headers.common['Authorization'] = 'Bearer ' + token;
+      axios.defaults.headers.common['Authorization'] = `Bearer ${token}`;
     },
+    
     setRole(state, role) {
       state.role = role;
       localStorage.setItem('role', role);
+    },
+    clearAuth(state) {
+      state.token = null;
+      state.role = null;
+      localStorage.removeItem('token');
+      localStorage.removeItem('role');
+      delete axios.defaults.headers.common['Authorization'];
     },
   },
   actions: {
@@ -44,10 +58,40 @@ const store = createStore({
 
         commit('setToken', response.data.token);
         commit('setRole', response.data.role);
-        return response.data; // Retorne os dados para uso no componente
+        return response.data;  // Retorna os dados para o componente
       } catch (error) {
-        throw error; // Lançar o erro para ser tratado no componente
+        throw error;  // Lança o erro para ser tratado no componente
       }
+    },
+
+    // Verifica se o usuário está autenticado e se o token é válido
+    async checkAuth({ state, dispatch }) {
+      console.log('Verificando token:', state.token); // Verifique se o token está sendo carregado corretamente
+    
+      if (state.token) {
+        try {
+          const response = await axios.get('http://localhost:8000/api/check-token', {
+            headers: {
+              Authorization: `Bearer ${state.token}`,
+            },
+          });
+    
+          if (response.data.valid) {
+            return true; // Token é válido
+          } else {
+            dispatch('logout');
+            return false; // Token inválido
+          }
+        } catch (error) {
+          dispatch('logout');
+          return false; // Se a requisição falhar, desloga o usuário
+        }
+      }
+      return false; // Se não houver token
+    },
+    
+    logout({ commit }) {
+      commit('clearAuth');
     },
   },
 });
