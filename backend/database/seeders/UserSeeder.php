c8b9713069bf0ae303f12e3908745090bc1273fd
Matheuskocotem <kocotemmatheus@gmail.com>
2024-11-06 12:26:18 -0300

    seed

--- backend/database/seeders/UserSeeder.php
+++ backend/database/seeders/UserSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use Illuminate\Database\Seeder;
 use App\Models\User;
 use Illuminate\Support\Facades\Hash;
+use Faker\Factory as Faker;
 
 class UserSeeder extends Seeder
 {
@@ -15,12 +16,28 @@ class UserSeeder extends Seeder
      */
     public function run()
     {
+        // Criar um usuário admin
         User::create([
             'name' => 'Admin User',
-            'email' => 'admin@example.com',
-            'cpf' => '12345678902',
-            'password' => Hash::make('password123'), 
+            'email' => 'admin4@example.com',
+            'cpf' => '12344608982', // CPF fixo para o admin
+            'password' => Hash::make('password123'),
             'role' => 'admin',
         ]);
+
+        // Criar 200 usuários com role 'user'
+        $faker = Faker::create();
+
+        foreach (range(10, 200) as $index) {
+            User::create([
+                'name' => $faker->name,
+                'email' => $faker->unique()->safeEmail,
+                'cpf' => $faker->numerify('###.###.###-##'), // Gerando CPF no formato correto
+                'password' => Hash::make('password123'), 
+                'role' => 'user', 
+                'created_at' => now(),
+                'updated_at' => now(),
+            ]);
+        }
     }
 }
