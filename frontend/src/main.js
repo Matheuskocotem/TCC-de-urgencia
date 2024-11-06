1e7001ae25ccd8348a08ba42986da487296cc31e
Matheuskocotem <kocotemmatheus@gmail.com>
2024-11-06 18:23:53 -0300

    main js arrumada

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -9,20 +9,9 @@ import "bootstrap";
 
 const app = createApp(App);
 
-// Use o router e o store
 app.use(router);
 app.use(store);
 
-// Verifique a autenticação assim que a aplicação iniciar
-store.dispatch('checkAuth').then((authenticated) => {
-  console.log('Usuário autenticado:', authenticated); // Verifique se a verificação de autenticação ocorre corretamente
-  if (!authenticated) {
-    router.push('/login');
-  }
-});
-
-
-// Configuração do Toast
 app.use(Toast, {
   transition: "Vue-Toastification__bounce",
   maxToasts: 20,
